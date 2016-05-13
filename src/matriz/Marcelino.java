e8360bd7ff694c9e72ff65e0e9431a3d1eb48ef5
marcelino <marcelino@marcelino-Inspiron-N5040>
2016-05-12 18:50:15 -0700

    Ejercicio 1 Terminado

--- src/matriz/Marcelino.java
+++ src/matriz/Marcelino.java
@@ -5,10 +5,48 @@
  */
 package matriz;
 
+import java.util.Random;
+
 /**
  *
  * @author marcelino
  */
 public class Marcelino {
-    
+    public void Ejercicio1()
+    {
+        int n = 0;
+        int llenar = 0;
+        int sumarPares = 0;
+        int multiplicarImpares = 0;
+        Random R = new Random();
+        
+        n = R.nextInt(9)+1;
+        llenar = R.nextInt(9)+1;
+        
+        
+        int[] Vector = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            Vector[i] = llenar;
+        }
+        
+        if (n % 2 == 0)
+        {
+            System.out.println("Es par");
+            for (int j = 0; j < n; j++) {
+               sumarPares += sumarPares + Vector[j]; 
+               System.out.println(sumarPares);
+            }
+        
+        }
+        else{
+            System.out.println("Es Impar");
+            for (int h = 0; h < n; h++) {
+                multiplicarImpares += multiplicarImpares * Vector[h];
+                System.out.println(multiplicarImpares);
+            }
+        }
+       
+        
+    }
 }
